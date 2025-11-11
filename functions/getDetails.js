da9132cb9fa58f2e182352a666bbdff2f63b8028
vitekform <vit.formanek.2022b@gypce.cz>
2025-11-11 20:16:51 +0100

    Initial commit

---
+++ functions/getDetails.js
@@ -0,0 +1,44 @@
+﻿export async function onRequest(context) {
+
+    const { request, env } = context;
+
+    const requestData = await request.json();
+    const ico = requestData.ico;
+
+    // Fetch data from ARES
+
+    fetch('https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/' + ico, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('JSON odpověď:', data);
+            if (data.ico !== ico) {
+                // How the f*** did this happen?
+                throw new Error('Could not obtain data because ICO mismatch happened. Who knows why...');
+            }
+            let companyName = data.obchodniJmeno;
+            let dic = data.dic;
+            let ulice = data.sidlo.nazevUlice;
+            let cisloPopisne = data.sidlo.cisloDomovni;
+            let kraj = data.sidlo.nazevKraje;
+            let mesto = data.sidlo.nazevObce;
+            let psc = data.sidlo.psc;
+            let pravniForma = data.pravniForma;
+            let regPlace = data.dalsiUdaje.spisovaZnacka;
+            let regDate = data.datumVzniku;
+            const dataResp = { "name": companyName, "dic": dic, "street": ulice, "address_num": cisloPopisne, "state": kraj, "city": mesto, "psc": psc, "legal_form": pravniForma, "reg_place": regPlace, "reg_date": regDate, "timestamp": Date.now() }
+            return new Response(JSON.stringify(dataResp));
+        })
+        .catch(error => {
+            return new Response(JSON.stringify(error));
+        });
+}
---
+++ src/App.jsx
@@ -0,0 +1,159 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    const [page, setPage] = useState(0)
+    const [ico, setIco] = useState("00000000")
+    const [companyName, setCompanyName] = useState("")
+    const [dic, setDic] = useState("")
+    const [street_and_number, setStreet_and_number] = useState("")
+    const [state, setState] = useState("")
+    const [city, setCity] = useState("")
+    const [zip, setZip] = useState("")
+    const [legalForm, setLegalForm] = useState("")
+    const [regDate, setRegDate] = useState("")
+    const [regPlace, setRegPlace] = useState("")
+    const [phone, setPhone] = useState("")
+    const [email, setEmail] = useState("")
+
+    function fetchDetailsFromICO() {
+        fetch("/api/getDetails", {body: JSON.stringify({"ico": ico}), method: "POST", headers: {
+            "Content-Type": "application/json"
+            }}).then((res) => {
+            let json = res.json();
+            setCompanyName(json.name);
+            setDic(json.dic);
+            let ulice = json.street;
+            let cislo = json.address_num;
+            setStreet_and_number(ulice + " " + cislo);
+            setState(json.state);
+            setCity(json.city);
+            setZip(json.psc);
+            setLegalForm(json.legal_form);
+            setRegDate(json.reg_date);
+            setRegPlace(json.reg_place);
+        })
+    }
+
+    function getHTMLContent(page) {
+        if (page === 0) {
+            return (
+                <>
+                    <div className="question-card">
+                        <label className="question">Prosím zadejte vaše IČO</label>
+                        <input
+                            type="text"
+                            onChange={e => {setIco(e.target.value)}}
+                            className="answer-input"
+                            inputMode="numeric"
+                            pattern="\d{8}"
+                            maxLength="8"
+                            placeholder="Vaše IČO"
+                            required
+                        />
+                    </div>
+                </>
+            )
+        }
+        else if (page === 1) {
+            return (
+                <>
+                    <div className="question-card">
+                        <label className="question">Jméno Firmy</label>
+                        <input
+                        type="text"
+                        onChange={e => {setCompanyName(e.target.value)}}
+                        required
+                        placeholder="Jméno Firmy"
+                        value={companyName}>
+                        </input>
+                    </div>
+                    <div className="question-card">
+                        <label className="question">DIČ Firmy</label>
+                        <input
+                            type="text"
+                            onChange={e => {setDic(e.target.value)}}
+                            required
+                            placeholder="DIČ Firmy"
+                            value={dic}>
+                        </input>
+                    </div>
+                    <div className="question-card">
+                        <label className="question">Ulice a Číslo popisné</label>
+                        <input
+                            type="text"
+                            onChange={e => {setStreet_and_number(e.target.value)}}
+                            required
+                            placeholder="Ulice a Číslo popisné"
+                            value={street_and_number}>
+                        </input>
+                    </div>
+                    <div className="question-card">
+                        <label className="question">Kraj</label>
+                        <input
+                            type="text"
+                            onChange={e => {setState(e.target.value)}}
+                            required
+                            placeholder="Kraj"
+                            value={state}>
+                        </input>
+                    </div>
+                    <div className="question-card">
+                        <label className="question">Obec</label>
+                        <input
+                            type="text"
+                            onChange={e => {setCity(e.target.value)}}
+                            required
+                            placeholder="Obec"
+                            value={city}>
+                        </input>
+                    </div>
+                    <div className="question-card">
+                        <label className="question">PSČ</label>
+                        <input
+                            type="text"
+                            onChange={e => {setZip(e.target.value)}}
+                            required
+                            placeholder="PSČ"
+                            value={zip}>
+                        </input>
+                    </div>
+                </>
+            )
+        }
+        else if (page === 2) {
+            <>
+                <div className="question-card">
+                    <label className="question">Kontaktní Telefon</label>
+                    <input
+                        type="tel"
+                        onChange={e => {setPhone(e.target.value)}}
+                        required
+                        placeholder="+420 777 777 777"
+                        value={phone}>
+                    </input>
+                </div>
+                <div className="question-card">
+                    <label className="question">Kontaktní E-Mail</label>
+                    <input
+                        type="email"
+                        onChange={e => {setEmail(e.target.value)}}
+                        required
+                        placeholder="pepa.novak@gmail.com"
+                        value={email}>
+                    </input>
+                </div>
+            </>
+        }
+    }
+  return (
+    <>
+        <div id={"form_container"}>
+        </div>
+    </>
+  )
+}
+
+export default App
